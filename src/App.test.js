4213345cc0cde5a4d47aba348c7312d6991459c8
Luis H <luishernandezcoding@gmail.com>
2023-01-31 12:59:38 -0600

    First commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello React', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/Hello React/i);
+  expect(element).toBeInTheDocument();
 });
