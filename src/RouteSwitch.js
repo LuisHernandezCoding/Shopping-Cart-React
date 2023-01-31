33694ac7e4aa1c946937fc46946b557485e248a5
LuisHernandezCoding <luishernandezcoding@gmail.com>
2023-01-31 17:35:45 -0600

    Added Navbar responsiveness

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import App from './pages/App';
 import Store from './pages/Store';
 import Contact from './pages/Contact';
@@ -7,7 +8,7 @@ import Navbar from './helper/Navbar';
 
 const RouteSwitch = () => (
   <BrowserRouter>
-    <Navbar active="home" />
+    <Navbar />
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/store" element={<Store />} />
--- src/helper/Navbar.js
+++ src/helper/Navbar.js
@@ -1,34 +1,51 @@
-const Navbar = (active) => (
-  <nav className="navbar has-background-primary">
-    <div className="container">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="/">
-          <img src="https://bulma.io/images/bulma-logo-white.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
-        </a>
-        <div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
-          <span />
-          <span />
-          <span />
+import React, { useState } from 'react';
+
+const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="/">
+            <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+          </a>
+          <button
+            onClick={() => {
+              setIsActive(!isActive);
+            }}
+            onKeyDown={() => {}}
+            type="button"
+            tabIndex={0}
+            className={`navbar-burger burger ${isActive ? 'is-active' : ''}`}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span />
+            <span />
+            <span />
+          </button>
+        </div>
+        <div id="navbarExampleTransparentExample" className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
+          <div className="navbar-start">
+            <a className="navbar-item" href="/">
+              Home
+            </a>
+            <a className="navbar-item" href="/store">
+              Store
+            </a>
+            <a className="navbar-item" href="/contact">
+              Contact
+            </a>
+            <a className="navbar-item" href="/about">
+              About
+            </a>
+          </div>
         </div>
       </div>
-      <div id="navbarExampleTransparentExample" className="navbar-menu">
-        <div className="navbar-start">
-          <a className={active === 'home' ? 'navbar-item is-active' : 'navbar-item'} href="/">
-            Home
-          </a>
-          <a className="navbar-item" href="/store">
-            Store
-          </a>
-          <a className="navbar-item" href="/contact">
-            Contact
-          </a>
-          <a className="navbar-item" href="/about">
-            About
-          </a>
-        </div>
-      </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default Navbar;
